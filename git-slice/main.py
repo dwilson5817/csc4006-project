34f2112142c7389ac7233516540b8c37601e118e
Dylan Wilson <dwilson5817@gmail.com>
2022-10-20 21:18:58 +0100

    Loop through each commit in the repository and list the files
    Create a simple prototype which starts at the currently checked out
    branch and traverses the repository through to the first commit.
    
    Some known problems with this implementation:
    
    - Only commits along the path are considered (for example, commits on
      other branches are not considered)
    - Works directly on specified directory - risk of damage as we are
      working on a version of the repo which may have uncommitted changes.
      This also will cause problems in a parallel implementation because
      threads will be checking out different commits.

--- git-slice/main.py
+++ git-slice/main.py
@@ -1,7 +1,11 @@
 import argparse
 import gettext
 import logging
+import os
 import sys
+
+import git
+from git import Repo
 
 from constants import project_constants
 
@@ -12,13 +16,41 @@ def runtime_info() -> None:
     """Output some system information which may be useful for debugging."""
 
     logging.info(project_constants.ascii_logo)
-    logging.info('')
     logging.info(_('Runtime information:'))
-    logging.info(_('  OS version: \t\t\t{}').format(sys.platform))
-    logging.info(_('  Python version: \t\t{}').format(sys.version))
-    logging.info(_('  Working directory: \t\t{}').format(args.working_directory))
-    logging.info(_('  Logging level: \t\t\t{}').format(args.log_level))
+    logging.info(_('  OS version: \t\t{}').format(sys.platform))
+    logging.info(_('  Python version: \t{}').format(sys.version))
+    logging.info(_('  GitPython version: \t{}').format(git.__version__))
+    logging.info(_('  Working directory: \t{}').format(args.working_directory))
+    logging.info(_('  Logging level: \t\t{}').format(args.log_level))
     logging.info('')
+
+
+def traverse_from_current_commit() -> None:
+    logging.debug(_('Beginning traversal from current commit'))
+    repo = Repo(args.working_directory)
+
+    original_branch = repo.active_branch.name
+    logging.info(_("Current branch is {}").format(original_branch))
+
+    for commit in repo.iter_commits():
+        # First line of commit message
+        commit_title = commit.message.splitlines()[0]
+
+        logging.info('')
+        logging.info(_('Checking out commit {} ({})'.format(commit, commit_title)))
+        logging.debug(_('Authored by {} on {}').format(commit.author, commit.authored_datetime))
+        logging.debug(_('Committed by {} on {}').format(commit.committer, commit.committed_datetime))
+
+        repo.git.checkout(commit)
+
+        logging.info('Listing files in directory:')
+        logging.info(' > ' + ', '.join(os.listdir(args.working_directory)))
+        logging.info('...done!')
+
+    logging.info('')
+    logging.info(_("Restoring original branch ({})").format(original_branch))
+
+    repo.git.checkout(original_branch)
 
 
 if __name__ == '__main__':
@@ -30,8 +62,11 @@ if __name__ == '__main__':
     parser.add_argument("-l", "--log-level", help="logging level, lowering this value will increase verbosity",
                         type=int, default=20)
 
+    # Ignore first argument from parsing as this will be the filename
     args = parser.parse_args(sys.argv[1:])
 
     logging.basicConfig(level=args.log_level)
 
     runtime_info()
+
+    traverse_from_current_commit()
